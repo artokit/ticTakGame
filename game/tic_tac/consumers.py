75f23c79f0c00eec8b129c04a475c2c1b6e16e56
artokit <kdsa3102@gmail.com>
2023-10-06 00:56:04 +0300

    user_steps

--- game/tic_tac/consumers.py
+++ game/tic_tac/consumers.py
@@ -1,5 +1,6 @@
 from channels.consumer import AsyncConsumer
 rooms = {}
+boards = {}
 
 
 class YourConsumer(AsyncConsumer):
@@ -14,18 +15,25 @@ class YourConsumer(AsyncConsumer):
             code = text_data['text'].split(':')[1]
             print(code)
             rooms[code] = [self]
-            await self.send({'type': 'websocket.send', 'text': "create:success"})
+            return await self.send({'type': 'websocket.send', 'text': "create:success"})
         
         if text_data['text'].startswith('join'):
             code = text_data['text'].split(':')[1]
+            
             if not rooms.get(code):
                 return await self.send({'type': 'websocket.send', 'text': 'error:Wrong code'})
+            
             if len(rooms[code]) == 1:
                 rooms[code].append(self)
                 for num, client in enumerate(rooms[code]):
                     c = 'o' if not num else 'x'
                     await client.send({'type': 'websocket.send', 'text': f'start_game:{code}:{c}'})
-                
+        
+        if text_data['text'].startswith('user_choice'):
+            code = text_data['text'].split(':')[1]
+            for i in rooms[code]:
+                if i != self:
+                    return await i.send({'type': 'websocket.send', 'text': text_data['text']})
 
     async def websocket_disconnect(self, event):
         pass
--- game/tic_tac/static/js/main.js
+++ game/tic_tac/static/js/main.js
@@ -3,11 +3,23 @@ const socket = new WebSocket('ws://127.0.0.1:8000/ws');
 let code = null;
 let choice = null;
 let myChoice = false;
+let boxes = document.querySelectorAll('.gamePanel__box');
 
 socket.onmessage = (event) => {
+	// console.log(event);
 	if (event.data == 'create:success') {
 		SwitchFunction(switchTo(-400));
 	}
+
+	if (event.data.startsWith('user_choice')) {
+		myChoice = true;
+		data = event.data.split(':');
+		let choiceEnemy = data[2];
+		let index = Number(data[3]);
+		let b = document.querySelectorAll('.gamePanel__box')[index];
+		createUserBox(choiceEnemy, b);
+	}
+
 	if (event.data.startsWith('start_game')) {
 		code = event.data.split(':')[1];
 		choice = event.data.split(':')[2];
@@ -19,6 +31,16 @@ socket.onmessage = (event) => {
 		SwitchFunction(switchTo(-500));
 	}
 };
+
+let getBoxIndex = (elem) => {
+	let index = 0;
+	for (let box of boxes) {
+		if (box == elem) {
+			return index;
+		}
+		index++;
+	}
+} 
 
 let SwitchFunction = (func) => {
 	let elems = document.querySelectorAll('.mainMenu div');
@@ -56,16 +78,21 @@ let switchTo = (percent) => {
 	return () => {mainMenuWrapper.style.transform = `translateY(${percent}%)`;};
 };
 
+let createUserBox = (c, panel_box) => {
+	let userBox = document.createElement('i');
+	userBox.className = `fa-solid fa-${c}`;
+	panel_box.appendChild(userBox);
+	userBox.style.animation = 'appearanceBoxUser .5s forwards';
+}
 
-let choiceGameBox = (elem) => {
+let choiceGameBox = (elem, i) => {
 	if (elem.innerHTML || !myChoice) {
 		return;
 	}
-	let userBox = document.createElement('i');
-	userBox.className = `fa-solid fa-${choice}`;
-	elem.appendChild(userBox);
-	userBox.style.animation = 'appearanceBoxUser .5s forwards';
-	socket.send("");  // Todo: Make send data about choice for enemy.
+
+	createUserBox(choice, elem);
+	let boxIndex = getBoxIndex(elem);
+	socket.send(`user_choice:${code}:${choice}:${boxIndex}`);
 	myChoice = false;
 };
 
@@ -73,6 +100,7 @@ for (let elem of document.querySelectorAll('.goBack')) {
 	elem.onclick = () => { SwitchFunction(switchTo(0)); };
 }
 
+let i = 0;
 for (let elem of document.querySelectorAll('.gamePanel__box')) {
-	elem.onclick = () => {choiceGameBox(elem)};
+	elem.onclick = () => {choiceGameBox(elem, i)};
 }
